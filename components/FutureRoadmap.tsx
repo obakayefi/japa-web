ea7cbee4cec78522298364a163d3189e47e7297d
obakayefi <founder.kayefistudios@protonmail.com>
2025-09-18 15:27:34 +0100

    Revert "make roadmap section responsive with new component"

--- components/FutureRoadmap.tsx
+++ components/FutureRoadmap.tsx
@@ -12,6 +12,8 @@ const FutureRoadmap = () => {
 
       <section className="flex sm:hidden flex-col gap-2">
         <MobileRoadmapCard>
+          <div>hello</div>
+          <div>hello</div>
         </MobileRoadmapCard>
         {/* <MobileRoadmapCard />
         <MobileRoadmapCard /> */}
